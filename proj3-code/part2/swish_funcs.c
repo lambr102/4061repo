789d807dc1ce81787997c5ebba317dc89120e3a5
logan Lambrecht <lambr102@umn.edu>
2025-11-12 02:26:55 +0000

    initial part2

--- proj3-code/part2/swish_funcs.c
+++ proj3-code/part2/swish_funcs.c
@@ -26,11 +26,54 @@
  * Returns 0 on success or -1 on error.
  */
 int run_piped_command(strvec_t *tokens, int *pipes, int n_pipes, int in_idx, int out_idx) {
-    // TODO Complete this function's implementation
+    if (in_idx != -1){
+        if (dup2(pipes[in_idx], STDIN_FILENO) == -1){
+            perror("dup2");
+            return -1;
+        }
+    }
+    if (out_idx != -1){
+        if (dup2(pipes[out_idx], STDIN_FILENO) == -1){
+            perror("dup2");
+            return -1;
+        }
+    }
+    if (run_command(&tokens) == -1){
+        printf("Error on Run Command");
+        return -1;
+    }
     return 0;
 }
 
 int run_pipelined_commands(strvec_t *tokens) {
-    // TODO Complete this function's implementation
+    int num_pipes = strvec_num_occurrences(&tokens, "|");
+    int *pipe_fds = malloc(2 * num_pipes * sizeof(int));
+
+    int start_idx = 0;
+    int end_idx = 0;
+
+
+    for (int i = 0; i < num_pipes; i ++){
+        strvec_t *temp_vec;
+        if (strvec_init(&temp_vec) == -1){
+            return -1;
+        }
+        for (int j = 0; strvec_get(&tokens, j) != "|"; j++ ){
+            end_idx ++;
+        }
+        if (strvec_slice(&tokens, &temp_vec, start_idx, end_idx) == -1){
+            return -1;
+        }
+        pid_t child = fork();
+        if (child == -1){
+            perror("fork");
+            free(pipe_fds);
+            return -1;
+        } else if (child == 0){
+            if (i == 0){
+                run_piped_command(temp_vec, pipe_fds, num_pipes, start_idx, end_idx);
+            }
+        }
+    }
     return 0;
 }
